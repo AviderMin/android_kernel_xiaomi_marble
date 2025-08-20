2a458b581e40a41c8ec509d72ee45f074a721c49
AviderMin <gms2917466108@gmail.com>
2025-08-20 23:36:05 +0800

    add build.sh and anykernel

---
+++ anykernel/anykernel.sh
@@ -0,0 +1,32 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=Melt-5.10-android12-GKI-SukiSU
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=marble
+device.name2=marblein
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+BLOCK=boot;
+IS_SLOT_DEVICE=1;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+dump_boot
+write_boot
