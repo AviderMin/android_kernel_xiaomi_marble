f454186df48b609376b505d7763c88fd7d6c9a1f
AviderMin <gms2917466108@gmail.com>
2025-08-21 19:10:52 +0800

    kernel: Make sus_mounts invisible only for apps that have sus_mounts umounted

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -22,7 +22,6 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_is_current_ksu_domain(void);
 bool susfs_hide_sus_mnts_for_all_procs = true; // hide sus mounts for all processes by default
 #endif
 
@@ -112,8 +111,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
-		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
+    if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID && susfs_hide_sus_mnts_for_all_procs)) {
 		return 0;
 #endif
 
@@ -152,8 +150,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
-		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
+    if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID && susfs_hide_sus_mnts_for_all_procs)) {
 		return 0;
 #endif
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
@@ -219,8 +216,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
-		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID && susfs_hide_sus_mnts_for_all_procs)) {
 		return 0;
 #endif
 
