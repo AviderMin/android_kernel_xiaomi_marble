1522ddb29c8db2e744f67f15e59c8c61082a2404
AviderMin <gms2917466108@gmail.com>
2025-08-21 19:12:20 +0800

    kernel: susfs_hide_sus_mnts_for_all_procs should be checked first for faster execution

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -111,7 +111,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-    if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID && susfs_hide_sus_mnts_for_all_procs)) {
+    if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID) {
 		return 0;
 #endif
 
@@ -150,7 +150,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-    if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID && susfs_hide_sus_mnts_for_all_procs)) {
+    if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID) {
 		return 0;
 #endif
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
@@ -216,7 +216,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID && susfs_hide_sus_mnts_for_all_procs)) {
+	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID) {
 		return 0;
 #endif
 
