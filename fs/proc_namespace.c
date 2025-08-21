70bcfa96d760803195d2d3811fb1b2d5b5315648
AviderMin <gms2917466108@gmail.com>
2025-08-21 19:07:42 +0800

    Revert kernel: Fix susfs_no_auto_add_ file toggle not working for SUS_MOUNT

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -112,7 +112,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt.mnt_root->d_inode->i_mapping->flags & BIT_SUS_MOUNT) &&
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
 		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
@@ -152,7 +152,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt.mnt_root->d_inode->i_mapping->flags & BIT_SUS_MOUNT) &&
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
 		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
@@ -219,7 +219,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt.mnt_root->d_inode->i_mapping->flags & BIT_SUS_MOUNT) &&
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
 		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
