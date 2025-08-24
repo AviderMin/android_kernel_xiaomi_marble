6b00f75e4a620757ef11416dbaedabf3b65a7ca6
AviderMin <gms2917466108@gmail.com>
2025-08-24 23:00:53 +0800

    clean rubbish

--- fs/overlayfs/util.c
+++ fs/overlayfs/util.c
@@ -143,7 +143,9 @@ bool ovl_dentry_weird(struct dentry *dentry)
 		return true;
 
 	return dentry->d_flags & (DCACHE_NEED_AUTOMOUNT |
-				  DCACHE_MANAGE_TRANSIT);
+				  DCACHE_MANAGE_TRANSIT |
+				  DCACHE_OP_HASH |
+				  DCACHE_OP_COMPARE);
 }
 
 enum ovl_path_type ovl_path_type(struct dentry *dentry)
